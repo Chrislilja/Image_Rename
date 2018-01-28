17a9d9da4ee8d954b9400a5fa47707de2a9ecc6a
Chrislilja <christopher.thomas.lilja@gmail.com>
2018-01-28 12:41:21 -0800

    First_commit

---
+++ ReNameATRON/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReNameATRON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] filePaths = Directory.GetFiles(@"C:\Users\Thomas\Pictures\FreeVideoToJPGConverter\Potato (2-15-2017 10-07-56 AM)");
+
+            int cnt = 1;
+            foreach (var item in filePaths)
+            {
+                System.IO.File.Move(item, cnt + ".jpg");
+                Console.WriteLine("renamed Image: " + cnt);
+                cnt++;
+            }
+
+        }
+    }
+}
